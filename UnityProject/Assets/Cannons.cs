49a389959aacda5e21986478fce28da889248df7
Tim Bouwman <tim210201.tb@gmail.com>
2020-09-28 18:41:42 +0200

    added ship

---
+++ UnityProject/Assets/Cannons.cs
@@ -0,0 +1,32 @@
+﻿//author: Tim Bouwman
+//Github: https://github.com/TimBouwman
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Cannons : MonoBehaviour
+{
+    #region Variables
+    
+    #endregion
+
+    #region Unity Methods
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Custom Methods
+    public void Fire()
+    {
+        Debug.Log("Fire: " + transform.name);
+    }
+    #endregion
+}
